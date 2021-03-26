d9e48004ad7abd5cfddd6813e683a6aa1572cb4c
alekseevayana <alekseevayana@bk.ru>
2021-03-26 15:48:32 -0700

    add test for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,7 @@ RSpec.describe User, type: :model do
   it "should create user with attributes" do
     expect(User.new(first_name: "Iana", last_name: "Alekseeva", email: "test@gmail.com", password: "123456")).to be_valid
   end
+  it "should call the method username and generate the proper username" do
+    expect(User.new(first_name: "Iana", last_name: "Alekseeva", email: "test@gmail.com", password: "123456").username).to eq("@test")
+  end
 end
---
+++ spec/requests/projects_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe "Projects", type: :request do
+  describe "GET /index" do
+    
+  end
+end
